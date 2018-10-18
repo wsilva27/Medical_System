5fb3ea17cbb26ab91c115001a0043d49233e0033
blacksmilez <43358528+blacksmilez@users.noreply.github.com>
2018-10-17 20:50:52 -0400

    Update conn_inc.php

--- Test1/conn_inc.php
+++ Test1/conn_inc.php
@@ -1,8 +1,8 @@
 <?php                                                                      
-// your_name_here                                                          
+// Ted Kim                                                          
 // Web Programming II - PHP                                                
 // Fall 2018                                                               
-// Practical Test 1                                                        
-$conn = mysql_connect("localhost", "root", "") or die(mysql_error());      
-$db = mysql_select_db("registration") or die(mysql_error());               
-?>                                                                         
+// Practical Test 1   
+$conn = mysqli_connect('localhost:8889', 'root', 'root') or die(mysql_error()); 
+$db = mysqli_select_db($conn, "registration") or die(mysql_error());     
+?>
