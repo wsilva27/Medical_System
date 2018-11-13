0a4adb8314c17300b2eb58e12671e293cc2ca44f
wsilva27 <welbertsilva27@gmail.com>
2018-11-12 22:15:28 -0500

    NOVEMBER 12

--- Test1/conn_inc.php
+++
@@ -1,8 +0,0 @@
-<?php                                                                      
-// Ted Kim                                                          
-// Web Programming II - PHP                                                
-// Fall 2018                                                               
-// Practical Test 1   
-$conn = mysqli_connect('localhost:8889', 'root', 'root') or die(mysql_error()); 
-$db = mysqli_select_db($conn, "registration") or die(mysql_error());     
-?>
