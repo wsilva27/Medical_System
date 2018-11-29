f77fa0dc0e09c71dadb249e3c9f0cee05667771e
blacksmilez <43358528+blacksmilez@users.noreply.github.com>
2018-11-28 20:55:37 -0500

    Add files via upload

---
+++ views/locations/index.php
@@ -0,0 +1,40 @@
+<?php
+    session_start();
+    require('../../conf/url.php');
+    $_SESSION['page_name'] = 'location';
+?>
+<!DOCTYPE html>
+<html>
+    <?php require('../../header.inc.php'); ?>
+    <body>
+        <?php 
+            require('../../nav.php');
+            require('breadcrumb.inc.php');
+        ?>
+        <div class="offset-md-2 col-md-10">
+            <div class="row">
+                <div class="col-12">
+                    <table id="table" class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>LOCATION ID</th>
+                                <th>ADDRESS</th>
+                                <th>CITY</th>
+                                <th>STATE</th>
+                                <th>ZIP</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                         </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 text-right">
+                    <button class="btn btn-outline-secondary btn-sm" onclick="loc.new();"><i class="fas fa-map"></i> New</button>
+                </div>
+            </div>
+        </div>
+        <?php require('../../bottom.php'); ?>
+    </body>
+</html>
---
+++ views/locations/profile.php
@@ -0,0 +1,83 @@
+<?php
+    session_start();
+    require('../../conf/url.php');
+    $_SESSION['page_name'] = 'location.profile';
+?>
+<!DOCTYPE html>
+<html>
+    <?php require('../../header.inc.php') ?>
+    <body>
+        <?php 
+            require('../../nav.php');
+            require('breadcrumb.inc.php');
+        ?>
+        <form id="profile" class="needs-validation" novalidate>
+            <div class="offset-2 col-10">
+                <div class="form-row">
+                    <div class="col-12 text-right">
+                        <button class="btn btn-outline-info btn-sm" type="button" onclick="loc.save();"><i class="fas fa-save"></i> Save</button>
+<!--                        <button class="btn btn-outline-danger btn-sm" type="button">Remove</button>-->
+                        <button class="btn btn-outline-secondary btn-sm" type="button" onclick="window.location='./';"><i class="fas fa-list-ol"></i> List</button>
+                    </div>
+                </div>
+
+                <input type="hidden" id="idx" value="<?php echo $_SESSION['idx']; ?>"/>
+                <div class="form-row">
+                    <div class="col-12">
+                        <div class="alert alert-info"><i class="fas fa-edit"></i> LOCATION INFO</div>
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="col-12">
+                        <label for="address">ADDRESS</label>
+                        <input type="text" id="address" name="address" class="form-control" required />
+                        <div class="valid-feedback">
+                            Looks good!
+                        </div>
+                        <div class="invalid-feedback">
+                            Please choose a address.
+                        </div>
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="col-4">
+                        <label for="city">CITY</label>
+                        <input type="text" id="city" name="city" class="form-control" required />
+                        <div class="valid-feedback">
+                            Looks good!
+                        </div>
+                        <div class="invalid-feedback">
+                            Please choose a city.
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <label for="state">STATE</label>
+                        <select id="state" name="state" class="form-control" required></select>
+                        <div class="valid-feedback">
+                            Looks good!
+                        </div>
+                        <div class="invalid-feedback">
+                            Please choose a title of specialty.
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <label for="zip">ZIP</label>
+                        <input type="text" id="zip" name="zip" class="form-control" required />
+                        <div class="valid-feedback">
+                            Looks good!
+                        </div>
+                        <div class="invalid-feedback">
+                            Please choose a title of specialty.
+                        </div>
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="col-12">
+                        <div id="msg" class="alert alert-primary" hidden></div>
+                    </div>
+                </div>
+            </div>
+        </form>
+        <?php require('../../bottom.php'); ?>
+    </body>
+</html>
