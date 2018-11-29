646fa062ded97c5405ae7cbaac48baf9465450f3
blacksmilez <43358528+blacksmilez@users.noreply.github.com>
2018-11-28 20:47:34 -0500

    Create url.php

---
+++ conf/url.php
@@ -0,0 +1,6 @@
+<?php
+//    $base_URL = ($_SERVER['HTTPS'] == 'on') ? 'https://' : 'http://';
+    $base_URL = 'http://';
+    $base_URL .= $_SERVER['HTTP_HOST'];
+    $base_URL .= "/CIS485_FINAL";
+?>
