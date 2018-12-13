14ff5210affc34f7c4a63262cb99b108b389c000
blacksmilez <43358528+blacksmilez@users.noreply.github.com>
2018-12-13 08:14:14 -0500

    Delete get.department.profile.php

--- api/get.department.profile.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start();
-    require_once '../lib/department.profile.php';
-    $res = array('data' => department\profile\get($_SESSION['idx']));
-    echo json_encode($res);
-?>
